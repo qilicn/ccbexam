be380cf3deb5ac9b7d90a59ea39a62bff6438417
qilicn <qilicn@hotmail.com>
2013-08-11 21:21:32 +0800

    更改错误的包搜索路径

--- src/java/com/ccb/dl/ccbexam/MainModule.java
+++ src/java/com/ccb/dl/ccbexam/MainModule.java
@@ -15,7 +15,7 @@ import org.nutz.mvc.ioc.provider.ComboIocProvider;
 @Modules(scanPackage = true)
 @IocBy(type=ComboIocProvider.class,
         args={"*org.nutz.ioc.loader.json.JsonLoader","app.js",
-        "*org.nutz.ioc.loader.annotation.AnnotationIocLoader","com.ccb.dl.mmisii"})
+        "*org.nutz.ioc.loader.annotation.AnnotationIocLoader","com.ccb.dl.ccbexam"})
 
 public class MainModule {
     
