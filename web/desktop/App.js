13d2bc0edd28e96c52a246eaefd4e14b199ad67f
yangxin <yangxin@yangxin-PC>
2013-08-17 20:12:16 +0800

    为了跟踪，保存一下

--- web/desktop/App.js
+++ web/desktop/App.js
@@ -24,50 +24,62 @@ Ext.define('MyDesktop.App', {
     
     //获取用户session信息
     getSessStore: function(){
+        var me = this;
         var Store = Ext.create('ccb.exam.store.userSessInfo',{});
         Store.load();
+
         if( Store.getCount() === 0){
             this.userSessInfo = null;
+            return;
         }
         //取出用户信息
-        this.userSessInfo = Store.getAt(0);
-        console.log(this.userSessInfo);
+        var retModel = Store.getAt(0);
+        this.userSessInfo = Ext.create('ccb.exam.model.user',retModel.get('retVal'));
+        //rv : 用户的功能列表
+        //后台数据库将字段全部小写，在此转换
+        var rv = retModel.get('retVal').shortcuts;
+        var shortcuts = new Array();
+        Ext.Array.each(rv,function(item){
+            item.iconCls = item.iconcls;
+            shortcuts.push(item);
+        })
         //创建功能列表的store
-        this.funcStore = Ext.create('ccb.exam.store.FuncStore');
-        var funcs = this.userSessInfo.get('shortcuts');
-        this.funcStore.insert(0,funcs);
-        this.funcStore.sync();
-        console.log(this.funcStore);
+        this.funcStore = Ext.create('ccb.exam.store.FuncStore',{data:shortcuts});     
     },
     init: function() {
         
         //判断用户是否通过验证
+        /*
         this.getSessStore();
         if( this.userSessInfo === null){
             Ext.Msg.alert('未登录用户','您未经过登录验证或已超时，请重新登陆',function(){
                 window.location.href = exam_golbal.baseUrl + '/desktop/login.html';
             })
-        }          
+        }         
+        */
         this.callParent();
         // now ready...
     },
 
     getModules : function(){
-        var Funcs = new Array();
-        this.funcStore.each(function(record){
-            console.log(recode);
+        //利用功能列表进行填充
+        var Modules = new Array();
+        this.funcStore.each(function(item){
+            var module = Ext.create(item.get('module'));
+            Modules.push(module);
         },this);
-        return [
-//            new MyDesktop.VideoWindow(),
-//            //new MyDesktop.Blockalanche(),
-//            new MyDesktop.SystemStatus(),
-//            new MyDesktop.GridWindow(),
-//            new MyDesktop.TabWindow(),
-//            new MyDesktop.AccordionWindow(),
-//            new MyDesktop.Notepad(),
-//            new MyDesktop.BogusMenuModule(),
-//            new MyDesktop.BogusModule()
-        ];
+        return Modules;
+//        return [
+////            new MyDesktop.VideoWindow(),
+////            //new MyDesktop.Blockalanche(),
+////            new MyDesktop.SystemStatus(),
+////            new MyDesktop.GridWindow(),
+////            new MyDesktop.TabWindow(),
+////            new MyDesktop.AccordionWindow(),
+//           new MyDesktop.Notepad(),
+////            new MyDesktop.BogusMenuModule(),
+////            new MyDesktop.BogusModule()
+//        ];
     },
 
     getDesktopConfig: function () {
@@ -100,7 +112,7 @@ Ext.define('MyDesktop.App', {
         var me = this, ret = me.callParent();
 
         return Ext.apply(ret, {
-            title: this.userSessInfo.get('userName'),
+            title: this.userSessInfo.get('username'),
             iconCls: 'user',
             height: 300,
             toolConfig: {
